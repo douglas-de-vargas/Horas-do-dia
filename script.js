bb627c2902116e6fabcdf3a65d7a2b0f4810dc02
douglassgbr@gmail.com <douglassgbr@gmail.com>
2023-09-04 09:03:00 -0300

    atu

--- script.js
+++ script.js
@@ -1,18 +1,24 @@
 const saudacao = document.querySelector('#saudacao');
 const horaAtual = document.querySelector('#horario');
+const secondsHtml = document.querySelector('#seconds');
 const image = document.querySelector('#image');
 const diaDaSemana1 = document.querySelector('#dia-da-semana');
 
-const newDate = new Date()
+const nD = new Date();
 
-const diaDaSemana = newDate.getDay()
+const diasDaSemana = ['Domingo', 'Segunda-Feira', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado'];
 
-const diasDaSemana = ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado'];
-const day = diasDaSemana[diaDaSemana];
-const hora = newDate.getHours();
-const minutes = newDate.getMinutes();
-const minutosForm = minutes < 10 ? "0" + minutes : minutes;
-const seconds = newDate.getSeconds();
+const horas = nD.getHours() < 10 ? '0' + nD.getHours() : nD.getHours();
 
-diaDaSemana1.innerText = day
-horaAtual.innerHTML = `${hora}:${minutosForm}:${seconds}`
+const minutos = nD.getMinutes() < 10 ?
+  '0' + nD.getMinutes() : nD.getMinutes();
+
+const seconds = nD.getSeconds() < 10 ? '0' + nD.getSeconds() : nD.getSeconds();
+
+saudacao.innerHTML = 'Bom dia!'
+
+diaDaSemana1.innerText = diasDaSemana[nD.getDay()];
+
+horaAtual.innerHTML = `${horas}:${minutos}`;
+
+secondsHtml.innerHTML = `:${seconds}`;
