b79935248ec1a9998638bc2bcc805b645bdf7a5c
douglassgbr@gmail.com <douglassgbr@gmail.com>
2023-09-04 09:37:23 -0300

    j

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+const body = document.querySelector('.body');
 const saudacao = document.querySelector('#saudacao');
 const horaAtual = document.querySelector('#horario');
 const secondsHtml = document.querySelector('#seconds');
@@ -8,17 +9,29 @@ const nD = new Date();
 
 const diasDaSemana = ['Domingo', 'Segunda-Feira', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado'];
 
-const horas = nD.getHours() < 10 ? '0' + nD.getHours() : nD.getHours();
+const hours = nD.getHours() < 10 ? '0' + nD.getHours() : nD.getHours();
 
-const minutos = nD.getMinutes() < 10 ?
+const minutes = nD.getMinutes() < 10 ?
   '0' + nD.getMinutes() : nD.getMinutes();
 
 const seconds = nD.getSeconds() < 10 ? '0' + nD.getSeconds() : nD.getSeconds();
 
-saudacao.innerHTML = 'Bom dia!'
+if (hours >= 6 && hours <= 11) {
+  saudacao.innerHTML = 'Bom Dia!';
+  body.classList.add('dia');
+} else if (hours >= 12 && hours <= 17) {
+  saudacao.innerHTML = 'Boa Tarde!';
+  body.classList.add('tarde');
+} else if (hours >= 18) {
+  saudacao.innerHTML = 'Boa Noite!';
+  body.classList.add('noite');
+} else {
+  saudacao.innerHTML = 'Boa Madrugada!'
+  body.classList.add('madrugada');
+};
 
-diaDaSemana1.innerText = diasDaSemana[nD.getDay()];
+diaDaSemana1.innerHTML = diasDaSemana[nD.getDay()];
 
-horaAtual.innerHTML = `${horas}:${minutos}`;
+horaAtual.innerHTML = `${hours}:${minutes}`;
 
 secondsHtml.innerHTML = `:${seconds}`;
