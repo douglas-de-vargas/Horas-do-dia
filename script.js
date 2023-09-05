654c9ecc907d155efce8d899ab118ad75280107b
douglassgbr@gmail.com <douglassgbr@gmail.com>
2023-09-05 10:22:47 -0300

    refatorando

--- script.js
+++ script.js
@@ -1,47 +1,56 @@
-const main = document.querySelector('.main')
 const body = document.querySelector('.body');
+const main = document.querySelector('.main');
 const saudacao = document.querySelector('#saudacao');
+const diaDaSemana1 = document.querySelector('#dia-da-semana');
 const horaAtual = document.querySelector('#horario');
 const secondsHtml = document.querySelector('#seconds');
-const image = document.querySelector('#image');
-const diaDaSemana1 = document.querySelector('#dia-da-semana');
 const imageDay = document.querySelector('#image');
 
 function attDate() {
-  const nD = new Date();
-
+  const nd = new Date();
+  const horas = nd.getHours();
+  const minutos = nd.getMinutes();
+  const segundos = nd.getSeconds();
   const diasDaSemana = ['Domingo', 'Segunda-feira', 'Terça-feira', 'Quarta-feira', 'Quinta-feira', 'Sexta-feira', 'Sábado'];
 
-  const hours = nD.getHours() < 10 ? '0' + nD.getHours() : nD.getHours();
+  let periodo = '';
+  iconId = '';
+  let corVariavel = '';
+  let imageSrc = '';
 
-  const minutes = nD.getMinutes() < 10 ?
-    '0' + nD.getMinutes() : nD.getMinutes();
-
-  const seconds = nD.getSeconds() < 10 ? '0' + nD.getSeconds() : nD.getSeconds();
-
-  if (hours >= 6 && hours <= 11) {
-    saudacao.innerHTML = `Bom Dia! <i class="bi bi-sun"></i>`;
-    body.classList.add('dia');
-    imageDay.src ="/manha.jpg";
-  } else if (hours >= 12 && hours <= 17) {
-    saudacao.innerHTML = `Boa Tarde! <i class="bi bi-brightness-high"></i>`;
-    body.classList.add('tarde');
-    imageDay.src ="/tarde.jpg";
-  } else if (hours >= 18) {
-    saudacao.innerHTML = `Boa Noite! <i class="bi bi-moon-stars"></i>`;
-    body.classList.add('noite');
-    imageDay.src ="/noite.jpg";
+  if (horas >= 6 && horas <= 11) {
+    periodo = 'manha';
+    iconId = 'sun';
+    corVariavel = '--corManha';
+    imageSrc = '/mamha.jpg';
+  } else if (horas >= 12 && horas <= 17) {
+    periodo = 'tarde';
+    iconId = 'sun';
+    corVariavel = '--corTarde';
+    imageSrc = '/tarde.jpg';
+  } else if (horas >= 18) {
+    periodo = 'noite';
+    iconId = 'moon-stars';
+    corVariavel = '--corNoite';
+    imageSrc = '/noite.jpg';
   } else {
-    saudacao.innerHTML = `Boa Madrugada! <i class="bi bi-moon-stars"></i>`
-    body.classList.add('madrugada');
-    imageDay.src ="/noite.jpg";
+    periodo = 'madrugada';
+    iconId = 'moon-stars';
+    corVariavel = '--corMadrugada';
+    imageSrc = '/noite.jpg';
   };
 
-  diaDaSemana1.innerHTML = diasDaSemana[nD.getDay()];
+  body.classList.add(periodo);
+  imageDay.style.borderColor = 'var(' + corVariavel + ')';
 
-  horaAtual.innerHTML = `${hours}:${minutes}`;
+  saudacao.innerHTML = `Boa ${periodo.charAt(0).toUpperCase() + periodo.slice(1)}! <i class='bi bi-${iconId}'></i>`;
 
-  secondsHtml.innerHTML = `:${seconds}`;
+  diaDaSemana1.innerHTML = diasDaSemana[nd.getDay()];
+
+  horaAtual.innerHTML = `
+  ${horas < 10 ? '0' : ''}${horas}:${minutos < 10 ? '0' : ''}${minutos}`;
+
+  secondsHtml.innerHTML = `:${segundos < 10 ? '0' : ''}${segundos}`;
 
   setTimeout(() => {
     main.classList.remove('load')
