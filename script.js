3bc9b3385120fdfbc5aff0ca61a066fa861e6f49
s1ns3ro <douglassgbr@gmail.com>
2023-10-01 22:14:53 -0300

    att css

--- script.js
+++ script.js
@@ -1,98 +1,103 @@
-const body = document.querySelector(".body");
-const main = document.querySelector(".main");
-const saudacao = document.querySelector("#saudacao");
-const saudacaoIcon = document.querySelector(".bi");
-const diaMes = document.querySelector("#dia-mes");
-const horaAtual = document.querySelector("#horario");
-const secondsHtml = document.querySelector("#seconds");
-const imageDay = document.querySelector("#image");
+const body = document.querySelector('.body')
+const main = document.querySelector('.main')
+const saudacao = document.querySelector('#saudacao')
+const saudacaoIcon = document.querySelector('.bi')
+const diaMes = document.querySelector('#dia-mes')
+const horaAtual = document.querySelector('#horario')
+const secondsHtml = document.querySelector('#seconds')
+const imageDay = document.querySelector('#image')
+const sectionImage = document.querySelector('#section-image')
 
 function attDate() {
-  const nd = new Date();
-  const diaAtual = nd.getDate();
-  const horas = nd.getHours().toString().padStart(2, "0");
-  const minutos = nd.getMinutes().toString().padStart(2, "0");
-  const segundos = nd.getSeconds().toString().padStart(2, "0");
+  const nd = new Date()
+  const diaAtual = nd.getDate().toString().padStart(2, '0')
+  const horas = 9 // nd.getHours().toString().padStart(2, '0')
+  const minutos = nd.getMinutes().toString().padStart(2, '0')
+  const segundos = nd.getSeconds().toString().padStart(2, '0')
   const diasDaSemana = [
-    "Domingo",
-    "Segunda-feira",
-    "Terça-feira",
-    "Quarta-feira",
-    "Quinta-feira",
-    "Sexta-feira",
-    "Sábado",
-  ];
+    'Dom.',
+    'Seg.',
+    'Ter.',
+    'Quar.',
+    'Quin.',
+    'Sex.',
+    'Sáb.',
+  ]
 
   const mesesDoAno = [
-    "Janeiro",
-    "Fevereiro",
-    "Março",
-    "Abril",
-    "Maio",
-    "Junho",
-    "Julho",
-    "Agosto",
-    "Setembro",
-    "Outubro",
-    "Novembro",
-    "Dezembro",
-  ];
+    'Jan.',
+    'Fev.',
+    'Mar.',
+    'Abr.',
+    'Mai.',
+    'Jun.',
+    'Jul.',
+    'Ago.',
+    'Set.',
+    'Out.',
+    'Nov.',
+    'Dez.',
+  ]
 
-  let periodo = "";
-  let iconId = "";
-  let corVariavel = "";
-  let imageSrc = "";
+  let periodo = ''
+  let deg = ''
+  let iconId = ''
+  let corVariavel = ''
+  let imageSrc = ''
 
   if (horas >= 6 && horas <= 11) {
-    periodo = "manha";
-    iconId = "bi-sun";
-    corVariavel = "--corManha";
-    imageSrc = "assets/manha.jpg";
+    periodo = 'manha'
+    deg = 'diadeg'
+    iconId = 'bi-sun'
+    corVariavel = '--corManha'
+    imageSrc = 'assets/manha.jpg'
   } else if (horas >= 12 && horas <= 17) {
-    periodo = "tarde";
-    iconId = "bi-sun";
-    corVariavel = "--corTarde";
-    imageSrc = "assets/tarde.jpg";
+    periodo = 'tarde'
+    deg = 'diadeg'
+    iconId = 'bi-sun'
+    corVariavel = '--corTarde'
+    imageSrc = 'assets/tarde.jpg'
   } else if (horas >= 18 && horas <= 23) {
-    periodo = "noite";
-    iconId = "bi-moon-stars";
-    corVariavel = "--corNoite";
-    imageSrc = "assets/noite.jpg";
+    periodo = 'noite'
+    deg = 'noitedeg'
+    iconId = 'bi-moon-stars'
+    corVariavel = '--corNoite'
+    imageSrc = 'assets/noite.jpg'
   } else if (horas >= 0 && horas <= 5) {
-    periodo = "madrugada";
-    iconId = "bi-moon-stars";
-    corVariavel = "--corMadrugada";
-    imageSrc = "assets/noite.jpg";
+    periodo = 'madrugada'
+    deg = 'noitedeg'
+    iconId = 'bi-moon-stars'
+    corVariavel = '--corMadrugada'
+    imageSrc = 'assets/noite.jpg'
   }
 
-  body.classList.add(periodo);
+  main.classList.add(periodo)
+  sectionImage.classList.add(deg)
 
-  imageDay.style.borderColor = "var(" + corVariavel + ")";
-
-  saudacaoIcon.style.textShadow = "1px 1px 1px var(" + corVariavel + ")";
-
-  if (periodo === "manha") {
-    saudacao.innerHTML = `Bom Dia!`;
+  if (periodo === 'manha') {
+    saudacao.innerHTML = `Bom Dia!`
   } else {
     saudacao.innerHTML = `Boa ${
       periodo.charAt(0).toUpperCase() + periodo.slice(1)
-    }!`;
+    }!`
   }
 
-  saudacaoIcon.classList.add(iconId);
+  saudacaoIcon.classList.add(iconId)
 
-  diaMes.innerHTML = `${diasDaSemana[nd.getDay()]}, ${diaAtual} de ${mesesDoAno[nd.getMonth()]} de ${nd.getFullYear()}`;
+  diaMes.innerHTML = `${diasDaSemana[nd.getDay()]} ${diaAtual} de ${
+    mesesDoAno[nd.getMonth()]
+  } de ${nd.getFullYear()}`
 
-  horaAtual.innerHTML = `${horas}:${minutos}`;
+  horaAtual.innerHTML = `${horas}:${minutos}`
 
-  secondsHtml.innerHTML = `:${segundos}`;
+  secondsHtml.innerHTML = `:${segundos}`
 
-  imageDay.src = imageSrc;
-  imageDay.alt = `Imagem da ${periodo}.`;
+  imageDay.src = imageSrc
+  imageDay.alt = `Imagem da ${periodo}.`
 
   setTimeout(() => {
-    main.classList.remove("load");
-  }, 2000);
+    main.classList.remove('load')
+  }, 2000)
 }
 
-setInterval(attDate, 500);
+setInterval(attDate, 500)
